ace468bbad5b261d8d935d018086aafeccf800ff
Rakshak R.Hegde <rakshakhegde@gmail.com>
2017-06-23 00:56:46 +0530

    fix: ui margins - width still broken

--- sample/src/main/java/com/azoft/carousellayoutmanager/sample/CustomZoomPostLayoutListener.kt
+++ sample/src/main/java/com/azoft/carousellayoutmanager/sample/CustomZoomPostLayoutListener.kt
@@ -3,8 +3,6 @@ package com.azoft.carousellayoutmanager.sample
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.support.v4.view.ViewCompat
-import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import com.azoft.carousellayoutmanager.CarouselLayoutManager
@@ -14,7 +12,7 @@ import com.github.florent37.expectanim.ExpectAnim
 class CustomZoomPostLayoutListener : CarouselLayoutManager.PostLayoutListener {
 
 	override fun transformChild(child: View, itemPositionToCenterDiff: Float, orientation: Int): ItemTransformation? {
-		Log.i("CustomZoomPostLayout", "transformChild : ${child.height} : ${ViewCompat.isLaidOut(child)}")
+//		Log.i("CustomZoomPostLayout", "transformChild : ${child.height} : ${ViewCompat.isLaidOut(child)} : $itemPositionToCenterDiff")
 		if (!ViewCompat.isLaidOut(child)) {
 			return null
 		}
@@ -34,17 +32,17 @@ class CustomZoomPostLayoutListener : CarouselLayoutManager.PostLayoutListener {
 
 		child.setTag(R.string.expectanim_view_id, currentAnim)
 
-		holder.mItemViewBinding.cItem1.text = String.format("%.2f | %.2f", itemPositionToCenterDiff, percent)
-		holder.mItemViewBinding.cItem2.text = String.format("%.2f | %.2f", itemPositionToCenterDiff, percent)
+		holder.mItemViewBinding.cItem1.text = String.format("%.2f | %.2f", percent, itemPositionToCenterDiff)
+		holder.mItemViewBinding.cItem2.text = String.format("%.2f | %.2f", percent, itemPositionToCenterDiff)
 
 		val grayscale: Int = (255 - percent * 10).toInt()
 		holder.mItemViewBinding.cardview.cardBackgroundColor = ColorStateList.valueOf(Color.rgb(grayscale, grayscale, grayscale))
 
-		holder.mItemViewBinding.rootlayout.transform<RecyclerView.LayoutParams> {
-			val margin: Int = (100 * Math.abs(itemPositionToCenterDiff) / 2).toInt()
-			setMargins(margin, 0, margin, 0)
-			marginStart = margin
-		}
+//		holder.mItemViewBinding.rootlayout.transform<ViewGroup.LayoutParams> {
+//			width = (600 + 100 * (1 - Math.abs(itemPositionToCenterDiff) / 2)).toInt()
+//			height = (100 + 50 * (1 - Math.abs(itemPositionToCenterDiff) / 2)).toInt()
+//			Log.i("CustomZoomPostLayout", "$itemPositionToCenterDiff : ${child.height} : ${child.top} : $width : $height")
+//		}
 
 		return null
 	}
